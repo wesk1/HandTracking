8ed65ea24e1d00a77016a4e7dd407f1dee341393
wesk1 <wesk18@yahoo.com>
2024-11-11 15:27:06 -0800

    Remove other files

--- handtracking.py
+++ handtracking.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
-
+import pyautogui as pag
+import numpy as np
 
 # Initialize Mediapipe Hand solution
 mp_hands = mp.solutions.hands
@@ -13,7 +14,7 @@ hands = mp_hands.Hands(static_image_mode=False,
 mp_drawing = mp.solutions.drawing_utils
 
 #open the camera
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 
 # error check to make sure the camera is open
@@ -21,6 +22,10 @@ if not cap.isOpened():
     print("Error")
     exit()
 
+#Set the Screen resolution (width, height)
+screen_width, screen_height = pag.size()
+
+mouseDown = False
 
 #Main loop
 while True:
@@ -39,11 +44,8 @@ while True:
     # Process the RGB frame with MediaPipe Hands
     results = hands.process(rgb_frame)
 
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            # Draw landmarks
-            mp_drawing.draw_landmarks(frame, hand_landmarks,mp_hands.HAND_CONNECTIONS)
-
+    #frame resolution
+    frame_height, frame_width, _ = frame.shape
 
     # Draw the hand annotations on the frame.
     if results.multi_hand_landmarks:
@@ -51,11 +53,44 @@ while True:
             #Draw landmarks
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
+            index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+            thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+
+            #Get the midpoint between the thumb and index finger
+            midpoint_x = (index_finger_tip.x + thumb_tip.x) / 2
+            midpoint_y = (index_finger_tip.y + thumb_tip.y) / 2
+
+            #Get the distance between the thumb and index finger
+            distance = np.sqrt((index_finger_tip.x - thumb_tip.x)**2 + (index_finger_tip.y - thumb_tip.y)**2)
+
+            if distance < 0.1 and mouseDown == False:
+                # mouse down
+                pag.mouseDown()
+                mouseDown = True
+            if distance > 0.3 and mouseDown == True:
+                # mouse up
+                pag.mouseUp()
+                mouseDown = False
+
+            if mouseDown:
+                # draw a circle at the midpoint with radius 10
+                cv2.circle(frame, (int(midpoint_x * frame_width), int(midpoint_y * frame_height)), 10, (0, 255, 0), -1)
+
+            else:
+                # draw a circle at the midpoint with radius 10
+                cv2.circle(frame, (int(midpoint_x * frame_width), int(midpoint_y * frame_height)), 10, (0, 255, 0), 1)
+
+            # Map the position to the screen resolution
+            x_mapped = np.interp(midpoint_x, (0, 1), (0, screen_width))
+            y_mapped = np.interp(midpoint_y, (0, 1), (0, screen_height))
+
+            # Set the mouse position
+            pag.moveTo(x_mapped, y_mapped, duration=0.1)
+
     # Display the resulting frame
-    cv2.imshow("Frame", frame)
+    cv2.imshow("Medipipe Hands", frame)
     cv2.waitKey(1)
-
-
+# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
 
